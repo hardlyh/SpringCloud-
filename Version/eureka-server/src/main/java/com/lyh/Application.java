7c255937e182006fd7932e5ff608559e9bd855ab
lyh <380056867@qq.com>
2020-07-30 19:02:48 +0800

    Inin EurekaServer

---
+++ Version/eureka-server/src/main/java/com/lyh/Application.java
@@ -0,0 +1,15 @@
+package com.lyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
