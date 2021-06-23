8d6902d7ce227047813ba6953de4e3ee2adf412f
lyh <380056867@qq.com>
2021-06-23 17:59:31 +0800

    配置文件测试

--- SpringCloud/springcloud-notice/src/main/java/notice/NoticeApplication.java
+++ SpringCloud/springcloud-notice/src/main/java/notice/NoticeApplication.java
@@ -1,13 +1,11 @@
 package notice;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-@MapperScan("notice.mapper")
 public class NoticeApplication {
 
 	public static void main(String[] args) {
